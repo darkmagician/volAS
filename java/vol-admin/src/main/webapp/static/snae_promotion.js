3d72acfb03549d33daeb7ca5cb4cd1f2d23bb2d6
scott <flyincloud@gmail.com>
2015-03-11 19:41:02 +0800

    split page for promotion editor

---
+++ java/vol-admin/src/main/webapp/static/snae_promotion.js
@@ -0,0 +1,158 @@
+	var dtheaderToolbar = [ {
+			text : '添加',
+			iconCls : 'icon-add',
+			handler : function() {
+			}
+		}, {
+			text : '编辑',
+			iconCls : 'icon-edit',
+			handler : function() {
+				var row = $('#draftPromotionMgr').datagrid('getSelected');
+				if (row) {
+					$('#promotionEditorInfo').text('');
+					$('#promotionEditor').dialog('open').dialog('setTitle', '编辑');
+					row.startTimeStr=formatDateBox(new Date(row.startTime));
+					row.endTimeStr=formatDateBox(new Date(row.endTime));
+					row.bonusExpirationTimeStr=formatDateBox(new Date(row.bonusExpirationTime));
+					enablePromotionEditor(true);
+					$('#promotionForm').form('load', row);
+					url = './rs/admin/promotion/update';
+					parentdg='#draftPromotionMgr';
+				}
+
+			}
+		}, '-', {
+			text : '删除',
+			iconCls : 'icon-remove',
+			handler : function() {
+				var row = $('#draftPromotionMgr').datagrid('getSelected');
+				if (row) {
+					$('#confirmInfo').text('');
+					$('#confirm').dialog('open').dialog('setTitle', '删除活动:'+row.name);
+					$('#confirmForm').form('load', row);
+					confirmurl = './rs/admin/promotion/delete';
+					afterConfirm=function(){
+						$('#draftPromotionMgr').datagrid('reload'); // reload the user data
+					}
+				}
+			}
+		} , '-', {
+			text : '激活',
+			iconCls : 'icon-save',
+			handler : function() {
+				var row = $('#draftPromotionMgr').datagrid('getSelected');
+				if (row) {
+					$('#confirmInfo').text('');
+					$('#confirm').dialog('open').dialog('setTitle', '激活活动:'+row.name);
+					$('#confirmForm').form('load', row);
+					confirmurl = './rs/admin/promotion/active/'+row.id;
+					afterConfirm=function(){
+						$('#draftPromotionMgr').datagrid('reload'); // reload the user data
+						$('#activePromotionMgr').datagrid('reload'); // reload the user data
+					}
+				}
+			}
+		}];
+		
+		 
+		var url;
+		var parentdg;
+				
+
+		var submitting=false;
+		function savePromotion() {
+			$('#promotionForm').form(
+					'submit',
+					{
+						onSubmit: function(param){
+							if(submitting) {
+								alert('正在提交中。。。 请不要重复提交。');
+								return false;
+							}
+							
+							if(!$(this).form('validate')){
+								return false;
+							}
+							 if(!(this.tenantId.value>0)){
+								 if(!currentTenantId){
+									 alert("请选择租户，再提交");
+									 return false;
+								 }
+								 this.tenantId.value=currentTenantId;
+								
+							 }
+							 param.startTime = parseDateToLong(this.startTimeStr.value);
+							 param.bonusExpirationTime = parseDateToLong(this.bonusExpirationTimeStr.value);
+							 param.endTime = parseDateToLong(this.endTimeStr.value);
+							 submitting=true;
+							 return true;
+						},
+						url : url,
+						success : function(result) {
+							submitting=false;
+							var result = eval('(' + result + ')');
+							if (result.code == 2001) {
+								$('#promotionEditor').dialog('close'); // close the dialog
+								$(parentdg).datagrid('reload'); // reload the user data
+							} else {
+								$('#promotionEditorInfo').text(
+										'错误: ' + result.message);
+							}
+						}
+					});
+		}	
+		
+		function enablePromotionEditor(enable){
+			var b = enable?'enable':'disable'
+			$('#promotionName').textbox(b);
+			$('#promotionVolumeType').combobox(b);
+			$('#promotionMaximum').numberbox(b);
+			$('#promotionStartTimeStr').datetimebox(b);
+			$('#promotionEndTimeStr').datetimebox(b);
+			$('#promotionBonusExpirationTimeStr').datetimebox(b);
+			$('#promotionDescription').textbox(b);
+			$('#promotionRule').attr("disabled",!enable);
+			$('#promotionOK').linkbutton(b);
+			
+		}
+		<!-- ************************************Active Promotion Editor ************************************ -->
+		function viewPromotion(){
+		
+				var row = $('#activePromotionMgr').datagrid('getSelected');
+				if (row) {
+					$('#promotionEditorInfo').text('');
+					$('#promotionEditor').dialog('open').dialog('setTitle', '查看');
+					
+					row.startTimeStr=formatDateBox(new Date(row.startTime));
+					row.endTimeStr=formatDateBox(new Date(row.endTime));
+					row.bonusExpirationTimeStr=formatDateBox(new Date(row.bonusExpirationTime));
+					enablePromotionEditor(false);
+					$('#promotionForm').form('load', row);
+
+				}
+		}
+
+		function editPromotion(){
+				var row = $('#draftPromotionMgr').datagrid('getSelected');
+				if (row) {
+					$('#promotionEditorInfo').text('');
+					$('#promotionEditor').dialog('open').dialog('setTitle', '编辑');
+					row.startTimeStr=formatDateBox(new Date(row.startTime));
+					row.endTimeStr=formatDateBox(new Date(row.endTime));
+					row.bonusExpirationTimeStr=formatDateBox(new Date(row.bonusExpirationTime));
+					enablePromotionEditor(true);
+					$('#promotionForm').form('load', row);
+					url = './rs/admin/promotion/update';
+					parentdg='#draftPromotionMgr';
+				}
+		}
+					
+		function createPromotion(){
+				$('#promotionEditorInfo').text('');
+				$('#promotionEditor').dialog('open').dialog('setTitle', '添加');
+				$('#promotionForm').form('clear');
+
+				enablePromotionEditor(true);
+				url = './rs/admin/promotion/add';
+				parentdg='#draftPromotionMgr';
+		}
