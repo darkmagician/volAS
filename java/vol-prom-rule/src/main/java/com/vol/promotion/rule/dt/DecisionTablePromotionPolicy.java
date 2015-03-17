82508399dcfbbd0419f5d5b7e66dd0dd0addef2e
unknown <weili5@L-SHC-00436352.corp.ebay.com>
2015-03-17 21:37:32 +0800

    initial com.vol.promotion.rule.dt.DecisionTablePromotionPolicy.evaluate(Promotion, Map<String, Object>)

--- java/vol-prom-rule/src/main/java/com/vol/promotion/rule/dt/DecisionTablePromotionPolicy.java
+++ java/vol-prom-rule/src/main/java/com/vol/promotion/rule/dt/DecisionTablePromotionPolicy.java
@@ -4,7 +4,9 @@
 package com.vol.promotion.rule.dt;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Random;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -20,6 +22,7 @@ import com.vol.promotion.rule.PromotionPolicySPI;
  */
 public class DecisionTablePromotionPolicy  extends AbstractPromotionPolicy implements PromotionPolicySPI {
 
+	private static final double MINOR_VALUE = 0.000000000001d;
 	private final ObjectMapper m = new ObjectMapper();
 	
 	
@@ -52,14 +55,88 @@ public class DecisionTablePromotionPolicy  extends AbstractPromotionPolicy imple
 		DecisionTableDefinition definition = toDefinition(promotion.getRule());
 		log.info("Validating: {}",definition);
 	}
+	
+	private Random random = new Random();
 
 	/* (non-Javadoc)
 	 * @see com.vol.common.service.PromotionPolicy#evaluate(com.vol.common.tenant.Promotion, java.util.Map)
 	 */
 	@Override
 	public Long evaluate(Promotion promotion, Map<String, Object> context) {
-		// TODO Auto-generated method stub
-		return null;
+		DecisionTableDefinition definition = toDefinition(promotion.getRule());
+		ColumnDefinition[] cols = definition.getCols();
+		String[][] data = definition.getData();
+		nextRow: for (int i = 0 ; i < data.length; i++) {
+			String[] row = data[i];
+			
+			for (int j = 0; j < cols.length - 1; j++) {
+				ColumnDefinition def = cols[j];
+
+				String grid = row[j];
+				if (grid == null || grid.isEmpty()) {
+					//no condition, means meet
+					continue;
+				}
+				
+				Object userValueRaw = context.get(def.getSrc());
+				if (ColumnDefinition.NUMBER == def.getType()) {
+					double userValue = ((Number) userValueRaw).doubleValue();
+					
+					if (grid.contains("~")) {
+						double low = Double.parseDouble(grid.substring(0, grid.indexOf("~")));
+						double high = Double.parseDouble(grid.substring(grid.indexOf("~") + 1));
+						
+						if (userValue > low - MINOR_VALUE && userValue <= high + MINOR_VALUE) {
+							//means meet
+						} else {
+							continue nextRow;
+						}
+						
+					} else {
+						
+						double expect = Double.parseDouble(grid);
+						if (userValue > expect - MINOR_VALUE && userValue <= expect + MINOR_VALUE) {
+							//means meet
+						} else {
+							continue nextRow;
+						}
+						
+					}
+					
+				} else if (ColumnDefinition.STRING == def.getType()) {
+					String userValue = (String) userValueRaw;
+					
+					if (grid.contains(",")) {
+						if (Arrays.asList(grid.split(",")).contains(userValue)) {
+							//means include
+						} else {
+							//means not match
+							continue nextRow;
+						}
+					} else {
+						//? case sensitive
+						if (grid.equals(userValue)) {
+						} else {
+							//not match
+							continue nextRow;
+						}
+					}
+				}
+			}
+			
+			// means meet all
+			//assume last column is the bonus
+			String bonusRaw = row[cols.length - 1];
+			if (bonusRaw.contains(",")) {
+				String[] split = bonusRaw.split(",");
+				return Long.parseLong(split[random.nextInt(split.length)]);
+			} else {
+				return Long.parseLong(bonusRaw);
+			}
+			
+		}
+				
+		return 0L;
 	}
 
 	/* (non-Javadoc)
--- java/vol-prom-rule/src/test/java/com/vol/promotion/rule/DecistionTableTest.java
+++ java/vol-prom-rule/src/test/java/com/vol/promotion/rule/DecistionTableTest.java
@@ -1,26 +1,107 @@
 package com.vol.promotion.rule;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import junit.framework.Assert;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 import com.vol.common.tenant.Promotion;
+import com.vol.promotion.rule.dt.ColumnDefinition;
+import com.vol.promotion.rule.dt.DecisionTableDefinition;
 
 /**
  * @author scott
- *
+ * 
  */
 public class DecistionTableTest extends BaseTest {
 
-	@Resource(name="DTPromotionPolicy")
+	@Resource(name = "DTPromotionPolicy")
 	PromotionPolicySPI promotionPolicySPI;
-	
+
 	@Test
-	public void testJSON(){
-		
-		Promotion promotion=new Promotion();
-		promotion.setRule("{\"cols\":[{\"title\":\"1111\",\"src\":\"username\",\"desc\":\"\",\"defaultVal\":\"1\",\"type\":\"N\"}],\"data\":[[\"1\",\"1\"]]}");
+	public void testJSON() {
+
+		Promotion promotion = new Promotion();
+		promotion
+				.setRule("{\"cols\":[{\"title\":\"1111\",\"src\":\"username\",\"desc\":\"\",\"defaultVal\":\"1\",\"type\":\"N\"}],\"data\":[[\"1\",\"1\"]]}");
 		promotionPolicySPI.validate(promotion);
-		
+
+	}
+
+	private final ObjectMapper m = new ObjectMapper();
+
+	@Test
+	public void testEvaluate() throws Exception {
+		Promotion promotion = new Promotion();
+		DecisionTableDefinition def = new DecisionTableDefinition();
+
+		List<ColumnDefinition> cols = new ArrayList<ColumnDefinition>();
+
+		List<String[]> data = new ArrayList<String[]>();
+		{
+			ColumnDefinition columnDefinition = new ColumnDefinition();
+			columnDefinition.setSrc("username");
+			columnDefinition.setType(ColumnDefinition.STRING);
+			cols.add(columnDefinition);
+		}
+		{
+			ColumnDefinition columnDefinition = new ColumnDefinition();
+			columnDefinition.setSrc("totalVolume");
+			columnDefinition.setType(ColumnDefinition.NUMBER);
+			cols.add(columnDefinition);
+		}
+		{
+			ColumnDefinition columnDefinition = new ColumnDefinition();
+			columnDefinition.setSrc("bonus");
+			columnDefinition.setType(ColumnDefinition.NUMBER);
+			cols.add(columnDefinition);
+		}
+		data.add(new String[] { "a1", "", "1000" });
+		data.add(new String[] { "a2", "", "2000" });
+		data.add(new String[] { "", "1000~2000", "3000" });
+		data.add(new String[] { "", "3000~4000", "4000" });
+		data.add(new String[] { "a5,a6", "0", "5000" });
+
+		def.setCols(cols.toArray(new ColumnDefinition[cols.size()]));
+		def.setData(data.toArray(new String[data.size()][]));
+		String rule = m.writeValueAsString(def);
+		System.out.println(rule);
+		promotion.setRule(rule);
+		HashMap<String, Object> context = new HashMap<String, Object>();
+		context.put("totalVolume", 0L);
+		{
+			context.put("username", "a1");
+			Assert.assertEquals(1000L,
+					promotionPolicySPI.evaluate(promotion, context).longValue());
+		}
+		{
+			context.put("username", "a2");
+			Assert.assertEquals(2000L,
+					promotionPolicySPI.evaluate(promotion, context).longValue());
+		}
+		{
+			context.put("username", "a3");
+			context.put("totalVolume", 1000L);
+			Assert.assertEquals(3000L,
+					promotionPolicySPI.evaluate(promotion, context).longValue());
+		}
+		{
+			context.put("username", "a4");
+			context.put("totalVolume", 3500L);
+			Assert.assertEquals(4000L,
+					promotionPolicySPI.evaluate(promotion, context).longValue());
+		}
+		{
+			context.put("username", "a5");
+			context.put("totalVolume", 0L);
+			Assert.assertEquals(5000L,
+					promotionPolicySPI.evaluate(promotion, context).longValue());
+		}
 	}
 }
